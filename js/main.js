c209c8dc55c3bb6b23eb8ab0aa51679b97f4c303
juliannesay <jmsay@wisc.edu>
2023-06-26 21:11:03 -0400

    completion of module 1-2 lesson 2

--- js/main.js
+++ js/main.js
@@ -1,119 +1,108 @@
 //* Map of income inequality per state */
-//declare map var in global scope
-//GOAL: Proportional symbols representing attribute values of mapped features
-//STEPS:
-//Step 1. Create the Leaflet map--already done in createMap()
-//Step 2. Import GeoJSON data--already done in getData()
-//Step 3. Add circle markers for point features to the map--already done in AJAX callback
-//Step 4. Determine the attribute for scaling the proportional symbols
-//Step 5. For each feature, determine its value for the selected attribute
-//Step 6. Give each feature's circle marker a radius based on its attribute value
+// Declare map variable globally so all functions have access
 var map;
 var minValue;
 
-//Step 1. Create the Leaflet map--already done in createMap()
-//function to instantiate the Leaflet map
-function createMap(){
-    //create the map
-    map = L.map('map', {
-        center: [38, -98],
-        zoom: 2
-    });
+// Step 1: Create the Leaflet map
+function createMap() {
+  // Create the map
+  map = L.map('map', {
+    center: [38, -98],
+    zoom: 2
+  });
 
-    //add OSM base tilelayer
-    var CartoDB_Voyager = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-        subdomains: 'abcd',
-        maxZoom: 20
-    }).addTo(map);
+  // Add CartoDB Voyager base tilelayer
+  var CartoDB_Voyager = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    subdomains: 'abcd',
+    maxZoom: 20
+  }).addTo(map);
 
-    //call getData function
-    getData();
-};
+  // Call getData function
+  getData();
+}
 
 function calculateMinValue(data) {
-    // create empty array to store all data values
-    var allValues = [];
-  
-    // loop through each feature
-    for (var feature of data.features) {
-        // loop through each year
-        for (var year = 2013; year <= 2019; year++) {
-            // get Gini Index value for current year
-            var value = feature.properties["Gini Index " + year]; // Corrected property name
-            // add value to array
-            allValues.push(value);
-        }
+  // Create empty array to store all data values
+  var allValues = [];
+
+  // Loop through each feature
+  for (var feature of data.features) {
+    // Loop through each year
+    for (var year = 2013; year <= 2019; year++) {
+      // Get Gini Index value for current year
+      var value = feature.properties["Gini Index " + year]; // Corrected property name
+      // Add value to array
+      allValues.push(value);
     }
-  
-    // get minimum value of the array
-    var minValue = Math.min(...allValues);
-  
-    return minValue;
+  }
+
+  // Get minimum value of the array
+  var minValue = Math.min(...allValues);
+
+  return minValue;
 }
 
-//Step 5. For each feature, determine its value for the selected attribute
-//calculate the radius of each proportional symbol
+// Step 5: For each feature, determine its value for the selected attribute
+// Calculate the radius of each proportional symbol
 function calcPropRadius(attValue) {
-    //constant factor adjusts symbol sizes evenly
-    var minRadius = 5;
-    //Flannery Apperance Compensation formula
-    var radius = 1.0083 * Math.pow(attValue/minValue, 0.5715) * minRadius;
+  // Constant factor adjusts symbol sizes evenly
+  var minRadius = 5;
+  // Flannery Apperance Compensation formula
+  var radius = 1.0083 * Math.pow(attValue / minValue, 0.5715) * minRadius;
 
-    return radius;
+  return radius;
 }
 
-//Step 3. Add circle markers for point features to the map--already done in AJAX callback
+// Step 3: Add circle markers for point features to the map
+function pointToLayer(feature, latlng) {
+  // Determine which attribute to visualize with proportional symbols
+  var attribute = "Gini Index 2019";
+
+  // Calculate the radius of each proportional symbol
+  var attValue = feature.properties[attribute];
+  var radius = calcPropRadius(attValue);
+
+  // Create circle marker layer
+  var geojsonMarkerOptions = {
+    radius: radius,
+    fillColor: "#1db8f5",
+    color: "#021a24",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.3
+  };
+
+  var layer = L.circleMarker(latlng, geojsonMarkerOptions);
+
+  // Build popup content string
+  var popupContent = "<p><b>State:</b> " + feature.properties.State + "</p>";
+  popupContent += "<p><b>Gini Index 2019:</b> " + feature.properties[attribute] + "</p>";
+  layer.bindPopup(popupContent);
+
+  return layer;
+}
+
+// Step 2: Import GeoJSON data
+function getData() {
+  // Load the data
+  fetch("data/IncomeInequality.geojson")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      // Calculate minimum data value
+      minValue = calculateMinValue(json);
+      // Call function to create proportional symbols
+      createPropSymbols(json);
+    });
+}
+
+// Step 4: Create proportional symbols
 function createPropSymbols(data) {
-   //Step 4. Determine the attribute for scaling the proportional symbols
-    var attribute = "Gini Index 2019";
-  
-    // create a GeoJSON layer with marker options and attach popups
-    L.geoJson(data, {
-        pointToLayer: function (feature, latlng) {
-          //Step 6. Give each feature's circle marker a radius based on its attribute value
-            var attValue = feature.properties[attribute];
-            var radius = calcPropRadius(attValue);
-            
-            var geojsonMarkerOptions = {
-                radius: radius,
-                fillColor: "#1db8f5",
-                color: "#021a24",
-                weight: 1,
-                opacity: 1,
-                fillOpacity: 0.3
-            };
-
-            return L.circleMarker(latlng, geojsonMarkerOptions);
-        },
-        onEachFeature: function (feature, layer) {
-            // create popup contents
-            var popupContent = "<p>Average Annual Income Inequality Estimated with the Gini Coefficient (US Census)</p>";
-            if (feature.properties) {
-                // loop to add feature property names and values to html string
-                for (var property in feature.properties) {
-                    popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
-                }
-                layer.bindPopup(popupContent);
-            }
-        }
-    }).addTo(map);
-}
-
-//Step 2. Import GeoJSON data--already done in getData()
-function getData(){
-    //load the data
-    fetch("data/IncomeInequality.geojson")
-        .then(function(response){
-            return response.json();
-        })
-        .then(function(json){
-            //calculate minimum data value
-            minValue = calculateMinValue(json);
-            //call function to create proportional symbols
-            createPropSymbols(json);
-        });
+  L.geoJson(data, {
+    pointToLayer: pointToLayer
+  }).addTo(map);
 }
 
 document.addEventListener('DOMContentLoaded', createMap);
-
--- js/module 2 tutorials.js
+++ js/module 2 tutorials.js
@@ -2,97 +2,124 @@
 var map;
 var minValue;
 
-//step 1 create map
-function createMap(){
+//step 1: create map
+function createMap() {
+  //create the map
+  map = L.map('map', {
+    center: [0, 0],
+    zoom: 2
+  });
 
-    //create the map
-    map = L.map('map', {
-        center: [0, 0],
-        zoom: 2
-    });
+  //add OSM base tilelayer
+  L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+  }).addTo(map);
 
-    //add OSM base tilelayer
-    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
-    }).addTo(map);
-
-    //call getData function
-    getData();
+  //call getData function
+  getData();
 };
 
-function calculateMinValue(data){
-    //create empty array to store all data values
-    var allValues = [];
-    //loop through each city
-    for(var city of data.features){
-        //loop through each year
-        for(var year = 1985; year <= 2015; year+=5){
-              //get population for current year
-              var value = city.properties["Pop_"+ String(year)];
-              //add value to array
-              allValues.push(value);
-        }
+function calculateMinValue(data) {
+  //create empty array to store all data values
+  var allValues = [];
+  //loop through each city
+  for (var city of data.features) {
+    //loop through each year
+    for (var year = 1985; year <= 2015; year += 5) {
+      //get population for current year
+      var value = city.properties["Pop_" + String(year)];
+      //add value to array
+      allValues.push(value);
     }
-    //get minimum value of our array
-    var minValue = Math.min(...allValues);
+  }
+  //get minimum value of our array
+  var minValue = Math.min(...allValues);
 
-    return minValue;
+  return minValue;
 }
 
 //calculate the radius of each proportional symbol
 function calcPropRadius(attValue) {
-    //constant factor adjusts symbol sizes evenly
-    var minRadius = 5;
-    //Flannery Apperance Compensation formula
-    var radius = 1.0083 * Math.pow(attValue/minValue,0.5715) * minRadius;
+  //constant factor adjusts symbol sizes evenly
+  var minRadius = 5;
+  //Flannery Apperance Compensation formula
+  var radius = 1.0083 * Math.pow(attValue / minValue, 0.5715) * minRadius;
 
-    return radius;
+  return radius;
+}
+
+//function to convert markers to circle markers
+function pointToLayer(feature, latlng) {
+  //Determine which attribute to visualize with proportional symbols
+  var attribute = "Pop_2015";
+
+  //create marker options
+  var options = {
+    fillColor: "#ff7800",
+    color: "#000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.8
+  };
+
+  //For each feature, determine its value for the selected attribute
+  var attValue = Number(feature.properties[attribute]);
+
+  //Give each feature's circle marker a radius based on its attribute value
+  options.radius = calcPropRadius(attValue);
+
+  //create circle marker layer
+  var layer = L.circleMarker(latlng, options);
+
+  //build popup content string starting with city
+  var popupContent = "<p><b>City:</b> " + feature.properties.City + "</p>";
+
+  //add formatted attribute to popup content string
+  var year = attribute.split("_")[1];
+  popupContent += "<p><b>Population in " + year + ":</b> " + feature.properties[attribute] + " million</p>";
+
+  //bind the popup to the circle marker with an offset
+  layer.bindPopup(popupContent, {
+    offset: new L.Point(0, -options.radius)
+  });
+
+  //return the circle marker to the L.geoJson pointToLayer option
+  return layer;
+};
+
+//Step 2: Import GeoJSON data
+function getData() {
+  //load the data
+  fetch("data/MegaCities.geojson")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      //calculate minimum data value
+      minValue = calculateMinValue(json);
+      //call function to create proportional symbols
+      createPropSymbols(json, map);
+    });
 }
 
 //Step 3: Add circle markers for point features to the map
-function createPropSymbols(data){
+function createPropSymbols(data, map) {
+  //Step 4: Determine which attribute to visualize with proportional symbols
+  var attribute = "Pop_2015";
 
-    //Step 4: Determine which attribute to visualize with proportional symbols
-    var attribute = "Pop_2015";
+  //create marker options
+  var geojsonMarkerOptions = {
+    fillColor: "#ff7800",
+    color: "#fff",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.8,
+    radius: 8
+  };
 
-    //create marker options
-    var geojsonMarkerOptions = {
-        fillColor: "#ff7800",
-        color: "#fff",
-        weight: 1,
-        opacity: 1,
-        fillOpacity: 0.8,
-        radius: 8
-    };
-
-    L.geoJson(data, {
-        pointToLayer: function (feature, latlng) {
-            //Step 5: For each feature, determine its value for the selected attribute
-            var attValue = Number(feature.properties[attribute]);
-
-            //Step 6: Give each feature's circle marker a radius based on its attribute value
-            geojsonMarkerOptions.radius = calcPropRadius(attValue);
-
-            //create circle markers
-            return L.circleMarker(latlng, geojsonMarkerOptions);
-        }
-    }).addTo(map);
-}
-
-
-//Step 2: Import GeoJSON data
-function getData(){
-    //load the data
-    fetch("data/MegaCities.geojson")
-        .then(function(response){
-            return response.json();
-        })
-        .then(function(json){
-            //calculate minimum data value
-            minValue = calculateMinValue(json);
-            //call function to create proportional symbols
-            createPropSymbols(json);
-        });
+  L.geoJson(data, {
+    pointToLayer: pointToLayer
+  }).addTo(map);
 }
 
 document.addEventListener('DOMContentLoaded', createMap);
